2cb8480b1568b361b0cee1ef825a037b133b3957
valentingenev <valentin_genev@abv.bg>
2022-05-07 12:55:05 +0300

    [+] react-tag-input and ReactTags; [=] poster is now always set to anonymous; [-] message's card-header;

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,11 @@
 export default function Message(props) {
-    const { postId, date, poster, message } = props
+    const { postId, date, message } = props
     // TODO: show how much time has passed
     const postDate = new Date(date)
 
     return (
         <li className="card mb-3" data-post={ postId }>
-            <div className="card-header">
-                <h2 className="card-title fs-6 mb-0">{ poster ? poster : 'John Doe' }</h2></div>
             <div className="card-body">
-
                 <div className="card-subtitle mb-2 text-muted">{ postDate.toLocaleString('en-GB') }</div>
                 <p className="card-text">{ message }</p>
             </div>
