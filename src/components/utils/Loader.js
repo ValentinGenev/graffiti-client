46c757d273feaf45594ce055fd12cca10c2e9cd8
valentingenev <valentin_genev@abv.bg>
2022-05-07 17:24:22 +0300

    [=] Field component turned to MessageField; [=] MessageField refactored; [=] MessageForm refactored, events moved around; [+] resetForm but not working; [+] SimpleSpinner

--- src/components/utils/Loader.js
+++ src/components/utils/Loader.js
@@ -1,8 +1,16 @@
-export default function Spinner(props) {
+export function Spinner(props) {
     return (
         <div className="d-flex align-items-center mb-2">
             <strong>{ props.children }</strong>
             <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+        </div>
+    )
+}
+
+export function SimpleSpinner() {
+    return (
+        <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
         </div>
     )
 }
