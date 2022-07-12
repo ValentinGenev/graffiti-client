5478895764a70007fe111cfea88dcc05dee0657b
valentingenev <valentin_genev@abv.bg>
2022-07-12 18:56:34 +0300

    [=] filter tag finished;

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,27 +1,32 @@
 import { useState, useEffect } from 'react'
+import { useSearchParams} from 'react-router-dom'
 import Pages from './Pages'
 import Messages from './Messages'
 import Pagination from './Pagination'
 import { getContent } from '../data/messages'
 
 export default function Wall() {
+    // TODO: introduce filters and remove the tags from here
+    const [searchParams] = useSearchParams();
     const [pagesCount, setPagesCount] = useState()
     const [pageIndex, setPageIndex] = useState(1)
     const [pages, setPages] = useState([undefined, <Messages key={ 1 } success={ false } />])
 
     const loadPage = async (index) => {
-        const pageWasLoaded = Boolean(pages[index] && pages[index].props.success)
-
-        if (pageWasLoaded) {
+        if (checkIfLoaded(pages, index) && !checkIfHasTag(searchParams)) {
             setPageIndex(index)
         }
         else {
-            setPages(pages => addPage(index, { success: false }, pages, loadPage))
+            setPages(pages => addPage(index, { success: false }, pages))
             setPageIndex(index)
 
-            const content = await getContent(index)
-            setPages(pages => addPage(index, content, pages))
+            const args = {
+                page: index,
+                tag: searchParams?.get('tagName')
+            }
+            const content = await getContent(args)
             if (content.success) {
+                setPages(pages => addPage(index, content, pages))
                 setPagesCount(content.pagination.pagesCount)
             }
             else {
@@ -30,10 +35,13 @@ export default function Wall() {
         }
     }
 
-    useEffect(() => loadPage(pageIndex), []);
+    useEffect(() => {
+        // TODO: this got messy, figure it out
+        loadPage(pageIndex)
+    }, [searchParams]);
 
     return (
-        <div className="Wall container me-4 p-0">
+        <div className="Wall container ms-0 me-4 p-0">
             <Pages
                 pages={ pages }
                 pageIndex={ pageIndex } />
@@ -58,4 +66,11 @@ function addPage(index, content, pages) {
             messages={ messages } />
 
     return newPages
+}
+
+function checkIfLoaded(pages, index) {
+    return Boolean(pages[index] && pages[index].props.success)
+}
+function checkIfHasTag(searchParams) {
+    return searchParams?.get('tagName')
 }
