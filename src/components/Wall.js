4911fc830e4ae3a63c06b9621d440facb0a9de7c
Valentin Genev <valentin_genev@abv.bg>
2022-07-17 15:12:41 +0300

    [=] refactored Wall's useEffect to have it's right dependencies; [=] used useCallback for the loadPage;

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import { useSearchParams} from 'react-router-dom'
 import Pages from './Pages'
 import Messages from './Messages'
@@ -7,12 +7,13 @@ import { getContent } from '../data/messages'
 
 export default function Wall() {
     // TODO: introduce filters and remove the tags from here
+    const isInitial = useRef(true)
     const [searchParams] = useSearchParams();
     const [pagesCount, setPagesCount] = useState()
     const [pageIndex, setPageIndex] = useState(1)
     const [pages, setPages] = useState([undefined, <Messages key={ 1 } success={ false } />])
 
-    const loadPage = async (index) => {
+    const loadPage = useCallback(async (index) => {
         if (checkIfLoaded(pages, index) && !checkIfHasTag(searchParams)) {
             setPageIndex(index)
         }
@@ -20,25 +21,23 @@ export default function Wall() {
             setPages(pages => addPage(index, { success: false }, pages))
             setPageIndex(index)
 
-            const args = {
+            const content = await getContent({
                 page: index,
                 tag: searchParams?.get('tagName')
-            }
-            const content = await getContent(args)
+            })
             if (content.success) {
                 setPages(pages => addPage(index, content, pages))
                 setPagesCount(content.pagination.pagesCount)
             }
-            else {
-                // TODO: add refresh button loadPage(index)
-            }
         }
-    }
+    }, [pages, searchParams])
 
     useEffect(() => {
-        // TODO: this got messy, figure it out
-        loadPage(pageIndex)
-    }, [searchParams]);
+        if (isInitial.current) {
+            loadPage(pageIndex)
+            isInitial.current = false
+        }
+    }, [isInitial, pageIndex, loadPage]);
 
     return (
         <div className="Wall container ms-0 me-4 p-0">
